49825872694ddd910f289da3a3c3f11cf91e466c
geordasche <george@lumindata.com>
2024-01-28 23:39:15 -0800

    Allow "Terraform destroy" to delete dataset contents. (#1)
    Co-authored-by: George Nyako <>

--- variables.tf
+++ variables.tf
@@ -17,6 +17,12 @@ variable "deletion_protection" {
   nullable    = false
 }
 
+variable "delete_contents_on_destroy" {
+  description = "If set to true, delete all the tables in the dataset when destroying the resource; otherwise, destroying the resource will fail if tables are present."
+  type        = bool
+  default     = false
+}
+
 variable "enable_services" {
   description = "Flag to enable or disable services in the Data Platform."
   type = object({
